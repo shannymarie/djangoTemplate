4f9be5377af2391ae159c51779c71bc1261612d3
shannymarie <fenico10@hotmail.nl>
2023-05-30 16:17:06 -0300

    made the tasks app and modified it with layout templates and navigation links

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -21,6 +21,7 @@ from django.urls import include, path
 urlpatterns = [
        path('hello/', include("hello.urls")),
     path('newyear/', include("newyear.urls")),
+     path('tasks/', include("tasks.urls")),
     path("admin/", admin.site.urls)
  
 ]
---
+++ tasks/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+tasks = ["man","vrouw","dier"]
+
+# Create your views here.
+def index(request):
+    return render(request, "tasks/index.html",{
+        "tasks": tasks
+    })
+
+def add(request):
+    return render(request, "tasks/add.html",{
+        "tasks": tasks
+    })
